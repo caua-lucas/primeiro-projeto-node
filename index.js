56719ac13595c4c8bf28d0b546b188c0020e08c5
caua-lucas <caual5397@gmail.com>
2025-11-06 15:32:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const uuid = require("uuid")
+const app = express()
+const port =  3001
+app.use(express.json())
+
+const users =[]
+
+
+
+app.get('/users', (request,response) => {
+    return response.json(users)
+})
+
+app.post('/users', (request,response) => {
+    const {name,age} = request.body
+    const user = { id:uuid.v4(),name,age}
+    users.push(user)
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id', (request,response) => {
+    const {id} = request.params
+    const {name,age} = request.body
+    const updateUser = {id,name,age}
+    
+    const index = users.findIndex(user => user.id === id )
+    if(index < 0){
+        return response.status(404).json({message: "User not found"})
+    }
+    users[index] = updateUser
+
+    return response.json(updateUser)
+})
+
+app.delete('/users/:id', (request,response) => {
+    const {id} = request.params
+    const index = users.findIndex(user => user.id === id )
+    if(index < 0){
+         return response.status(404).json({message: "User not found"})
+    }
+    users.splice(index,1)
+    return response.status(204).json("")
+})
+
+app.listen(port, ()=>{
+    console.log('🌅 Server started on ${port}')
+})
